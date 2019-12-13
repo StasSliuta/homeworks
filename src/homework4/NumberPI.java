f6b4f35acd0a5cd1a02f8030a9edd4cd9105edce
StasSliuta <57006766+StasSliuta@users.noreply.github.com>
2019-12-14 00:37:23 +0200

    Delete NumberPI.java

--- src/homework4/NumberPI.java
+++
@@ -1,7 +0,0 @@
-package homework4;
-
-public class NumberPI {
-    public static void main(String[] args) {
-        System.out.printf("%.1f%n%.2f%n%.3f%n%.4f%n%.5f", Math.PI,Math.PI,Math.PI,Math.PI,Math.PI);
-    }
-}
